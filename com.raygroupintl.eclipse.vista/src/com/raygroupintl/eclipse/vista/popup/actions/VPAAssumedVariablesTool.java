3bfa8d58e3f14d0467d19161e7749c2fa903e687
austundag <afsin.ustundag@us.pwc.com>
2013-05-15 07:53:18 -0400

    Changed the recursion level for Assumed Variables and Quit Type tools.

--- com.raygroupintl.eclipse.vista/src/com/raygroupintl/eclipse/vista/popup/actions/VPAAssumedVariablesTool.java
+++ com.raygroupintl.eclipse.vista/src/com/raygroupintl/eclipse/vista/popup/actions/VPAAssumedVariablesTool.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.raygroupintl.m.tool.OutputFlags;
 import com.raygroupintl.m.tool.ParseTreeSupply;
 import com.raygroupintl.m.tool.entry.MEntryToolResult;
+import com.raygroupintl.m.tool.entry.RecursionDepth;
 import com.raygroupintl.m.tool.entry.assumedvariables.AssumedVariables;
 import com.raygroupintl.m.tool.entry.assumedvariables.AssumedVariablesTool;
 import com.raygroupintl.m.tool.entry.assumedvariables.AssumedVariablesToolParams;
@@ -24,6 +25,7 @@ public class VPAAssumedVariablesTool extends VPATool {
 		params.addExpected("U");
 		params.addExpected("DT");
 		params.addExpected("DUZ");
+		params.getRecursionSpecification().setDepth(RecursionDepth.LABEL);
 		AssumedVariablesTool tool = new AssumedVariablesTool(params);
 		MEntryToolResult<AssumedVariables> result = tool.getResultForRoutines(selectedFileNames);
 		return result;
--- com.raygroupintl.eclipse.vista/src/com/raygroupintl/eclipse/vista/popup/actions/VPAQuitTypeTool.java
+++ com.raygroupintl.eclipse.vista/src/com/raygroupintl/eclipse/vista/popup/actions/VPAQuitTypeTool.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.raygroupintl.m.tool.CommonToolParams;
 import com.raygroupintl.m.tool.ParseTreeSupply;
 import com.raygroupintl.m.tool.entry.MEntryToolResult;
+import com.raygroupintl.m.tool.entry.RecursionDepth;
 import com.raygroupintl.m.tool.entry.quittype.QuitType;
 import com.raygroupintl.m.tool.entry.quittype.QuitTypeTool;
 
@@ -12,6 +13,7 @@ public class VPAQuitTypeTool extends VPATool {
 	@Override
 	protected MEntryToolResult<QuitType> getResult(ParseTreeSupply pts, List<String> selectedFileNames) {
 		CommonToolParams params = new CommonToolParams(pts);
+		params.getRecursionSpecification().setDepth(RecursionDepth.ALL);
 		QuitTypeTool tool = new QuitTypeTool(params);
 		MEntryToolResult<QuitType> result = tool.getResultForRoutines(selectedFileNames);
 		return result;
